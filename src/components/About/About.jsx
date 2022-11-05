9852473364e2d7c36c56f87a5a4bc27b9694b2e7
BarangayTagapo <brgytagapostarosa@gmail.com>
2022-11-05 13:58:57 +0800

    Your message

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -37,7 +37,7 @@ const About = () => {
             </aside>
             <div>
               <img src={chairman} alt='brgy chairman' />
-              <h4>Barangay Chairman. 
+              <h4 style="text-align:center">Barangay Chairman. 
                 ARTURO S. CATINDIG</h4>
             </div>
           </div>
